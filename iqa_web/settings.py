415def77a86ab75fcfea27a8641a85d06a8e0430
Yodsawadee Sornda <jan.yodsawadee@outlook.com>
2019-02-06 16:07:00 +0700

    whitenoise installed apps comment

--- iqa_web/settings.py
+++ iqa_web/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
